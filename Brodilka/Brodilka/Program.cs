f5e38561c823eced2588a6a1878d7231bc45ac61
101-105-109 <124188586+101-105-109@users.noreply.github.com>
2023-04-21 07:56:37 +0300

    Add files via upload

---
+++ Brodilka/Brodilka/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brodilka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            char[,] map = {
+            {'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'},
+            {'#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+            {'#','X',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+            {'#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+            {'#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+            {'#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+            {'#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+            {'#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+            {'#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+            {'#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+            {'#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','X','#'},
+            {'#','X',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+            {'#',' ',' ',' ',' ','X',' ',' ',' ',' ',' ',' ',' ','X','#'},
+            {'#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+            {'#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+            {'#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+            {'#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+            {'#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+            {'#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+            {'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'}
+            };
+
+            int axisX = 6, axisY = 6;
+            char[] bag = new char [1];  
+            while (true)
+            {
+                Console.SetCursorPosition(0, 25);
+                for (int i = 0; i < bag.Length; i++)
+                {
+                    Console.Write(bag[i] + " ");
+                }
+
+                Console.SetCursorPosition(0,0);
+
+                for (int i = 0; i < map.GetLength(0); i++)
+                {
+                    for (int j = 0; j < map.GetLength(1); j++)
+                    {
+                        Console.Write(map[i, j]);
+                    }
+                    Console.WriteLine();
+                }
+
+                Console.Write("Сумка:");
+
+
+                char hero = '@';
+                Console.SetCursorPosition(axisY, axisX);
+                Console.WriteLine(hero);
+
+                ConsoleKeyInfo consoleKey = Console.ReadKey();
+
+                switch (consoleKey.Key)
+                {
+                    case ConsoleKey.W:
+                    case ConsoleKey.UpArrow:
+                        if (map[axisX - 1, axisY] != '#')
+                        {
+                            axisX--;
+                        }
+                        break;
+                    case ConsoleKey.S:
+                    case ConsoleKey.DownArrow:
+                        if (map[axisX + 1, axisY] != '#')
+                        {
+                            axisX++;
+                        }
+                        break;
+                    case ConsoleKey.A:
+                    case ConsoleKey.LeftArrow:
+                        if (map[axisX, axisY - 1] != '#')
+                        {
+                            axisY--;
+                        }
+                        break;
+                    case ConsoleKey.D:
+                    case ConsoleKey.RightArrow:
+                        if (map[axisX, axisY + 1] != '#')
+                        {
+                            axisY++;
+                        }
+                        break;
+                }
+
+                if (map[axisX,axisY] == 'X') 
+                {
+                    map[axisX, axisY] = 'o';
+
+                    char[] temBag = new char[bag.Length+1];
+
+                    for (int i = 0; i < bag.Length; i++)
+                    {
+                        temBag[i] = bag[i];
+                    }
+                    temBag[temBag.Length-1] = 'X';
+                    bag = temBag;
+
+                }
+                Console.Clear();
+            }
+
+        }
+    }
+}
