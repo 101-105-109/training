2fffdaca27e4244de3c1ef1ee81d631b6de081e7
101-105-109 <124188586+101-105-109@users.noreply.github.com>
2023-04-24 07:12:11 +0300

    Add files via upload

---
+++ changeableBar/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace changeableBar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int MaxZn = 10;
+            int MinZn = 8;
+            int Mana = 8;
+
+            while (true)
+            {
+                changeableBar(MinZn, MaxZn, ConsoleColor.DarkRed, 0);
+                changeableBar(Mana, MaxZn, ConsoleColor.DarkBlue, 1);
+
+                Console.SetCursorPosition(0, 5);
+
+                Console.Write("Hp:");
+                MinZn = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Man:");
+                Mana = Convert.ToInt32(Console.ReadLine());
+
+                Console.ReadKey();
+                Console.Clear();
+
+            }
+        }
+        static void changeableBar(int MinZn, int MaxZn, ConsoleColor color, int positione)
+        {
+            string Bar = "";
+
+            for (int i = 0; i < MinZn; i++)
+            {
+                Bar += " ";
+            }
+
+            Console.Write("[");
+            Console.SetCursorPosition(0, positione);
+            Console.BackgroundColor = color;
+            Console.Write(Bar);
+            Console.BackgroundColor = default;
+
+            for (int i = MinZn; i < MaxZn; i++)
+            {
+                Bar += " ";
+            }
+
+            Console.Write(Bar + "]");
+        }
+    }
+}
---
+++ function/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Words();
+                Console.ReadKey();
+            }
+        }
+        static void RandomWords()
+        {
+            Random random = new Random();
+            int True = random.Next(0, 2);
+
+            if (True == 0)
+            {
+
+                Console.ForegroundColor = (ConsoleColor)random.Next(0, 16);
+                Console.BackgroundColor = (ConsoleColor)random.Next(0, 16);
+
+            }
+            else
+            {
+                Console.ResetColor();
+            }
+        }
+        static void Words()
+        {
+            bool isOpen = true;
+            int line, column;
+            char[,] words = {
+            {'ё','й','ц','у','к','е','н','г','ш','щ','з'},
+            {'д','л','о','р','п','а','в','ы','ф','ъ','х'},
+            {'ж','э','я','ч','с','м','и','т','ь','б','ю'},
+            {'1','2','3','4','5','6','7','8','9','0',' '}
+            };
+
+            Random random = new Random();
+
+            int number_of_characters = random.Next(0, words.Length);
+
+            while (isOpen)
+            {
+
+                for (int i = 0; i < number_of_characters; i++)
+                {
+                    line = random.Next(0, words.GetLength(0));
+                    column = random.Next(0, words.GetLength(1));
+                    Console.Write(words[line, column]);
+                    RandomWords();
+                }
+                Console.WriteLine();
+                isOpen = false;
+            }
+
+        }
+    }
+}
